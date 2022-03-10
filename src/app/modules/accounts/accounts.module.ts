91b449ee73d27e0fe8b763811f59acd040a2147b
Praveen Kumar <praveen@textmercato.com>
2022-03-10 07:15:29 +0530

    accounts module components initialized

--- src/app/modules/accounts/accounts.module.ts
+++ src/app/modules/accounts/accounts.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountsRoutingModule } from './accounts-routing.module';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { AccountShellComponent } from './pages/account-shell/account-shell.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent,
+    SigninComponent,
+    AccountShellComponent,
+    ProfileComponent,
+    PasswordResetComponent
+  ],
   imports: [
     CommonModule,
     AccountsRoutingModule
---
+++ src/app/modules/accounts/pages/account-shell/account-shell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-shell',
+  templateUrl: './account-shell.component.html',
+  styleUrls: ['./account-shell.component.scss']
+})
+export class AccountShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
