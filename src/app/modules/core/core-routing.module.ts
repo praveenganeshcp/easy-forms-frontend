207509e9d238a7b9fde056b2e06c5d3c967cc922
Praveen Kumar <praveen@textmercato.com>
2022-03-29 21:37:38 +0530

    core app shell setup completed

--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoreAppShellComponent } from './components/core-app-shell/core-app-shell.component';
+import { FormListComponent } from './pages/form-list/form-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CoreAppShellComponent,
+    children: [
+      {
+        path: 'forms',
+        component: FormListComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
