02b26729ae0f8371ce76ca00c1537c3984362590
Praveen Kumar <praveen@textmercato.com>
2022-04-03 09:53:52 +0530

    forgot password ui completed

--- src/app/modules/accounts/accounts.module.ts
+++ src/app/modules/accounts/accounts.module.ts
@@ -6,8 +6,9 @@ import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { AccountShellComponent } from './pages/account-shell/account-shell.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
+import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 import { SharedModule } from '../shared/shared.module';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SharedModule } from '../shared/shared.module';
     SigninComponent,
     AccountShellComponent,
     ProfileComponent,
-    PasswordResetComponent
+    PasswordResetComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
