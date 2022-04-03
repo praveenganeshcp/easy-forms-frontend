3f098cc8f2c391498ec7cb93f4513d2bc8d2b867
Praveen Kumar <praveen@textmercato.com>
2022-04-03 14:26:17 +0530

    common service specs added

--- src/app/modules/shared/services/common/common.service.spec.ts
+++ src/app/modules/shared/services/common/common.service.spec.ts
@@ -3,5 +3,21 @@ import { TestBed } from '@angular/core/testing';
 import { CommonService } from './common.service';
 
 describe('CommonService', () => {
-  
+  let service: CommonService | null;
+
+  beforeEach(() => {
+    service = new CommonService();
+  })
+
+  it('should return app name', () => {
+      expect(service?.getAppName()).toBe('Easy forms');
+  })
+
+  it('should return app slogan', () => {
+      expect(service?.getAppSlogan()).toBe('Create forms, collect data and export it at ease');
+  })
+
+  afterEach(() => { 
+    service = null;
+  })
 });
