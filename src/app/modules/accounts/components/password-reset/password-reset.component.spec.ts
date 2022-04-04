71185defc122b278ca3946b4bc4959ef99d03ad9
Praveen Kumar <praveen@textmercato.com>
2022-04-04 07:16:16 +0530

    specs added for password reset component

--- src/app/modules/accounts/components/password-reset/password-reset.component.spec.ts
+++ src/app/modules/accounts/components/password-reset/password-reset.component.spec.ts
@@ -1,25 +1,53 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PasswordResetComponent } from './password-reset.component';
 
 describe('PasswordResetComponent', () => {
-  let component: PasswordResetComponent;
   let fixture: ComponentFixture<PasswordResetComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PasswordResetComponent ]
-    })
-    .compileComponents();
-  });
+  let component: PasswordResetComponent;
+  let template: DebugElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PasswordResetComponent ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PasswordResetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    template = fixture.debugElement;
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  function getElement(queryString: string) {
+    return template.query(By.css(queryString)).nativeElement;
+  }
+
+  it('should render password reset form title', () => {
+    const formTitleElement: HTMLHeadingElement = getElement('.password-reset__form h2');
+    expect(formTitleElement).toBeInstanceOf(HTMLHeadingElement);
+    expect(formTitleElement.innerText).toBe('RESET PASSWORD');
+  })
+
+  it('should render input field to enter new password', () => {
+    const passwordField: HTMLInputElement = getElement(".password-reset__form input[name='password']");
+    expect(passwordField).toBeInstanceOf(HTMLInputElement);
+  })
+
+  it('should render input field to enter confirm new password', () => {
+    const confirmPasswordField: HTMLInputElement = getElement(".password-reset__form input[name='confirmPassword']");
+    expect(confirmPasswordField).toBeInstanceOf(HTMLInputElement);
+  })
+
+  it('should render button to perform save password action', () => {
+    const saveButtonElement: HTMLButtonElement = getElement('.password-reset__form button');
+    expect(saveButtonElement).toBeInstanceOf(HTMLButtonElement);
+    expect(saveButtonElement.innerText).toBe('SAVE PASSWORD');
+  })
+
+  it('should render link to signup page', () => {
+    const signupPageLink: HTMLAnchorElement = getElement('.password-reset__form p a');
+    expect(signupPageLink).toBeInstanceOf(HTMLAnchorElement);
+    expect(signupPageLink.getAttribute('routerLink')).toBe('/accounts/create-account');
+  })
 });
