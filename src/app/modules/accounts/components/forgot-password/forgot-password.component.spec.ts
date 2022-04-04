e50687142ab1c7eefb7a11524d3b29db7782bb5b
Praveen Kumar <praveen@textmercato.com>
2022-04-04 08:28:33 +0530

    specs added for forgot password component

--- src/app/modules/accounts/components/forgot-password/forgot-password.component.spec.ts
+++ src/app/modules/accounts/components/forgot-password/forgot-password.component.spec.ts
@@ -1,25 +1,48 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
 describe('ForgotPasswordComponent', () => {
-  let component: ForgotPasswordComponent;
-  let fixture: ComponentFixture<ForgotPasswordComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: ComponentFixture<ForgotPasswordComponent>;
+  let component: ForgotPasswordComponent;
+  let template: DebugElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ForgotPasswordComponent, ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ForgotPasswordComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    template = fixture.debugElement;
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  function getElement(queryString: string) {
+    return template.query(By.css(queryString)).nativeElement;
+  }
+
+  it('should render forgot password form title', () => {
+    const formTitleElement: HTMLHeadingElement = getElement('.forgot-password__form h2');
+    expect(formTitleElement).toBeInstanceOf(HTMLHeadingElement);
+    expect(formTitleElement.innerText).toBe('FORGOT PASSWORD');
+  })
+
+  it('should render input field to specify registered mail id', () => {
+    const mailIdField: HTMLInputElement = getElement(".forgot-password__form input[name='registeredMailId']");
+    expect(mailIdField).toBeInstanceOf(HTMLInputElement);
+  })
+
+  it('should render button to perform send password reset link action to input mailid', () => {
+    const sendLinkButton: HTMLButtonElement = getElement('.forgot-password__form button');
+    expect(sendLinkButton).toBeInstanceOf(HTMLButtonElement);
+    expect(sendLinkButton.innerText).toBe('SEND PASSWORD-RESET LINK');
+  })
+
+  it('should render link to signup page', () => {
+    const signupLink: HTMLAnchorElement = getElement('.forgot-password__form a');
+    expect(signupLink.getAttribute('routerLink')).toBe('/accounts/create-account');
+  })
 });
