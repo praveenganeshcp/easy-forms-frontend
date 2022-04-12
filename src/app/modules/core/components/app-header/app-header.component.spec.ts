f311e6c3dff66594e0062f7d53e862b0f0b1f944
Praveen Kumar <praveen@textmercato.com>
2022-04-12 11:42:37 +0530

    test specs added for header and app shell components

--- src/app/modules/core/components/app-header/app-header.component.spec.ts
+++ src/app/modules/core/components/app-header/app-header.component.spec.ts
@@ -3,4 +3,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppHeaderComponent } from './app-header.component';
 
 describe('AppHeaderComponent', () => {
+
+    it('should display the application name', () => {})
+
+    it('should display navigation links to home, trash and profile pages', () => {})
+
+    it('should provide option to logout', () => {})
 });
--- src/app/modules/core/components/core-app-shell/core-app-shell.component.spec.ts
+++ src/app/modules/core/components/core-app-shell/core-app-shell.component.spec.ts
@@ -3,23 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CoreAppShellComponent } from './core-app-shell.component';
 
 describe('CoreAppShellComponent', () => {
-  let component: CoreAppShellComponent;
-  let fixture: ComponentFixture<CoreAppShellComponent>;
+  it('should display application header that is consistent across all the routes', () => {})
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CoreAppShellComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CoreAppShellComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should provide the placeholder to create app components', () => {})
 });
